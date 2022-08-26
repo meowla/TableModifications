5d8eddfd5c73209b6545de10942998d6832469c0
meowla <redmooncow@163.com>
2022-08-26 19:32:00 +0800

    second

--- src/pages/head/index.js
+++ src/pages/head/index.js
@@ -36,10 +36,30 @@ export const Head=() => {
           width:300,
           render: (text,row,index)=>{
             return <Paragraph
+              id={(row.id=="-1")?"JimPlays":row.id}
               editable={{
                autoSize: false,
                onChange: (newVal)=>{
                   if(newVal == "") return
+                  if(row.id == "-1"){
+                    request.post('/feature/add-image-feature-config',{
+                      dataFormat: "",
+                      defaultValue: "false",
+                      featureName: newVal,
+                      remark: row.remark,
+                      type: "checkbox",
+                      imageVersion: "test1",
+                   })
+                   .then(()=>{
+                    request.post('/feature/get-image-feature-config',{
+                      imageVersion:'test1',
+                    })
+                     .then((res) =>{
+                        console.log(res);
+                        setData(res.data.data);
+                    });})
+                    return
+                  }
                   request.post('/feature/update-image-feature-config',{
                     dataFormat: "",
                     defaultValue: "false",
@@ -102,27 +122,29 @@ export const Head=() => {
         },
         {
           title:<Button type="link" onClick={()=>{
-//            const da1=[{id:"-1",featureName:"",remark:""}]
-//            data.forEach(item=>{
-//              da1.push(item)
-//            })
-//            setData(da1)
-              request.post('/feature/add-image-feature-config',{
-                dataFormat: "",
-                defaultValue: "false",
-                featureName: "新建属性("+Math.ceil(Math.random()*100000)+")",
-                imageVersion: "test1",
-                remark: "",
-                type: "checkbox"
-              })
-              .then(()=>{
-                 request.post('/feature/get-image-feature-config',{
-                 imageVersion:'test1',
-              })
-              .then((res) =>{
-                console.log(res);
-                setData(res.data.data);
-              });})
+           const da1=[{id:"-1",featureName:"",remark:""}]
+           data.forEach(item=>{
+             da1.push(item)
+           })
+           setData(da1)
+           setTimeout(() => {document.getElementById('JimPlays').getElementsByClassName('ant-typography-edit')[0].click()}, 100)
+ //          document.getElementById('JimPlaysFootball')?.click();
+              // request.post('/feature/add-image-feature-config',{
+              //   dataFormat: "",
+              //   defaultValue: "false",
+              //   featureName: "新建属性("+Math.ceil(Math.random()*100000)+")",
+              //   imageVersion: "test1",
+              //   remark: "",
+              //   type: "checkbox"
+              // })
+              // .then(()=>{
+              //    request.post('/feature/get-image-feature-config',{
+              //    imageVersion:'test1',
+              // })
+              // .then((res) =>{
+              //   console.log(res);
+              //   setData(res.data.data);
+              // });})
           }}>添加</Button>,
           dataIndex:'operate',
           width:100,
